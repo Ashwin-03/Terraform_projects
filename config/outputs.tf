8eed0c7be3c8de12af360274682e18254106e790
localashwin <localashwin@gmail.com>
2023-05-12 16:31:33 +0530

    updated output

--- config/outputs.tf
+++ config/outputs.tf
@@ -1,7 +1,7 @@
 output "rg_name" {
-  value = azurerm_resource_group.name
+  value = azurerm_resource_group.name.id
 }
 
 output "location" {
-  value = azurerm_resource_group.location
+  value = azurerm_resource_group.location.id
 }
