6a72b5cf5749d6f646c9808c83fcf5f5892bb291
localashwin <localashwin@gmail.com>
2023-05-12 17:11:51 +0530

    backend removed

--- Resource_group/main.tf
+++ Resource_group/main.tf
@@ -5,11 +5,12 @@ terraform {
       version = "~>3.0.2"
     }
   }
-  backend "local" {}
   required_version = ">=1.1.0"
 }
 
-provider "azurerm" {}
+provider "azurerm" {
+  features {}
+}
 
 resource "azurerm_resource_group" "my-rg"{
   name = var.resource_group_name
