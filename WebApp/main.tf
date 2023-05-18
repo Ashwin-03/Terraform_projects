e0a2398706ef13fb6ad535215937e907e3d10bf8
localashwin <localashwin@gmail.com>
2023-05-18 17:09:28 +0530

    asp servers added

---
+++ WebApp/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>3.0.2"
+    }
+  }
+  backend "azurerm" {}
+  required_version = ">=1.1.0"
+}
+
+resource "azurerm_app_service_plan" "myasp1" {
+  name = var.myasp-1
+  resource_group_name = var.myrg
+  location = var.mylocation
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service_plan" "myasp2" {
+  name = var.myasp-2
+  resource_group_name = var.myrg
+  location = var.mylocation
+  
+  sku_name = "P1v2"
+  os_type = "Windows"
+}
+
+resource "azurerm_app_service" "mylap" {
+  name = var.mylapp
+  resource_group_name = var.myrg
+  location = var.mylocation
+  service_plan_id = azurerm_app_service_plan.myasp1.id
+
+  site_config {}
+}
+
+resource "azurerm_windows_web_app" "mywap" {
+  name = var.mywapp
+  resource_group_name = var.myrg
+  location =  var.mylocation
+  service_plan_id = azurerm_app_service_plan.myasp2.id
+
+  site_config {}
+}
+
+
---
+++ WebApp/variables.tf
@@ -0,0 +1,23 @@
+variable "myrg" {
+  default = "my-vms"
+}
+
+variable "mylocation" {
+  default = "east us 2"
+}
+
+variable "myasp-1" {
+  default = "myasp1"
+}
+
+variable "myasp-2" {
+  default = "myasp2"
+}
+
+variable "mywapp" {
+  default = "Wappser"
+}
+
+variable "mylapp" {
+  default = "Lappser"
+}
