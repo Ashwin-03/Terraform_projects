d6de900990cdfe0e627512f078809717a88954a3
localashwin <localashwin@gmail.com>
2023-05-18 13:10:38 +0530

    output updated

--- VM/outputs.tf
+++ VM/outputs.tf
@@ -1,19 +1,19 @@
 output "myrg" {
-  default = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.rg.name
 }
 
 output "myloc" {
-  default = azurerm_resource_group.rg.location
+  value = azurerm_resource_group.rg.location
 }
 
 output "mynic" {
-  default = azurerm_network_interface.nic.id
+  value = azurerm_network_interface.nic.id
 }
 
 output "mypubip" {
-  default = azurerm_public_ip.pubip.id
+  value = azurerm_public_ip.pubip.id
 }
 
 output "myvm" {
-  default = azurerm_virtual_machine.vm.name
+  value = azurerm_virtual_machine.vm.name
 }
