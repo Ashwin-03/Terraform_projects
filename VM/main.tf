1993aa0716717797398013775025318574f146c7
localashwin <localashwin@gmail.com>
2023-05-18 13:12:46 +0530

    address prefix updated

--- VM/main.tf
+++ VM/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_subnet" "subnet" {
   name = var.mysubnet
   resource_group_name = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix = "10.2.1.0/24"
+  address_prefixes = "10.2.1.0/24"
 }
 
 resource "azurerm_public_ip" "pubip" {
